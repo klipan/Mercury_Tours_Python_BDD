a4547b61f1208844f2475cb2f283dacb83851b9f
KlipaN <nikola.klipa@yahoo.com>
2018-08-28 18:03:38 +0200

    commit sign-in steps

--- features/steps/register_step.py
+++ features/steps/register_step.py
@@ -7,12 +7,32 @@ def step_impl(context):
 @step('Click on Register tab')
 def step_impl(context):
     context.browser.click_on_element('link', 'REGISTER')
-@step('Fill out all required fields')
+@step('Fill out all required fields with valid credentials')
 def step_impl(context):
-    pass
+    context.browser.send_text_to_element('css', '[name=email]', 'Pobeda')
+    context.browser.send_text_to_element('css', '[name=password]', 'Hasta la Victoria Siempre')
+    context.browser.send_text_to_element('css', '[name=confirmPassword]', 'Hasta la Victoria Siempre')
 @step('Click Submit')
 def step_impl(context):
-    pass
+    context.browser.click_on_element('css','[type=image]')
 @step('Verify that submit is successful')
 def step_impl(context):
-    pass
+    assert context.browser.get_url() == 'http://newtours.demoaut.com/create_account_success.php'
+@step('Click on sign-in link')
+def step_impl(context):
+    context.browser.click_on_element('css', '[href="mercurysignon\.php"]')
+@step('Fill out username and password with valid credentials')
+def step_impl(context):
+    context.browser.send_text_to_element('css', '[name=userName]', 'Pobeda')
+    context.browser.send_text_to_element('css', '[name=password]', 'Hasta la Victoria Siempre')
+@step('Verify that login is successful')
+def step_impl(context):
+    assert context.browser.get_title() == 'Find a Flight: Mercury Tours:'
+@step('Fill out username and password with error credentials')
+def step_impl(context):
+    context.browser.send_text_to_element('css', '[name=userName]', 'SSS123')
+    context.browser.send_text_to_element('css', '[name=password]', '123FDEWT')
+@step('Verify that login is unsuccessful')
+def step_impl(context):
+    assert context.browser.get_title() == 'Sign-on: Mercury Tours'
+
